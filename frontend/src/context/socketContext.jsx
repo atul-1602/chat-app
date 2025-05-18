0ac23bf20a80fa4168dfa0af0b7565874815ccc4
TechTeam#12 <techteam12@TechTeam12s-MacBook-Pro.local>
2025-05-18 21:43:11 +0530

    socket url updated

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -19,7 +19,7 @@ export const SocketContextProvider = ({ children }) => {
             return
         }
     
-        const newSocket = io("http://localhost:8000", {
+        const newSocket = io("https://chat-app-msvy.onrender.com/", {
             auth: {
                 userId: authUser._id
             }
